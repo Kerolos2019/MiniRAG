b3f29ca965495aa5f59c9cae06cd8c65fc3e509d
kerolos yacoub <kerolos20140659@yahoo.com>
2026-01-29 21:21:21 +0200

    ugb

--- source/controllers/BaseController.py
+++ source/controllers/BaseController.py
@@ -8,11 +8,11 @@ class BaseController:
 
         self.app_settings = get_settings()
         
-    #     self.base_dir = os.path.dirname( os.path.dirname(__file__) )
-    #     self.files_dir = os.path.join(
-    #         self.base_dir,
-    #         "assets/files"
-    #     )
+        self.base_dir = os.path.dirname( os.path.dirname(__file__) )
+        self.files_dir = os.path.join(
+            self.base_dir,
+            "assets/files"
+        )
         
-    # def generate_random_string(self, length: int=12):
-    #     return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
+    def generate_random_string(self, length: int=12):
+        return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
