cf7bcb7a49e4f4d4d1e331ac3c5b48e84aa4d9f8
kerolos yacoub <kerolos20140659@yahoo.com>
2026-01-29 17:17:20 +0200

    first

---
+++ source/routes/data.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
+from fastapi.responses import JSONResponse
+import os
+from helpers.config import get_settings, Settings
+from controllers import DataController
+import aiofiles
+#from models import ResponseSignal
+import logging
+
+logger = logging.getLogger('uvicorn.error')
+
+data_router = APIRouter(
+    prefix="/api/v1/data",
+    tags=["api_v1", "data"],
+)
+
+@data_router.post("/upload/{project_id}")
+async def upload_data(project_id: str, file: UploadFile,
+                      app_settings: Settings = Depends(get_settings)):
+        
+    
+    # validate the file properties
+    data_controller = DataController()
+
+    is_valid,result_signal = data_controller.validate_uploaded_file(file=file)
+
+    if not is_valid:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": result_signal
+            }
+        )
+    else:
+        return True,result_signal
